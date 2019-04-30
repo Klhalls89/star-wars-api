6adfc9ae56d54d5f3cdc501dd56ed06262b1f3c1
Kristen Hallstrom <klhalls89@gmail.com>
2019-04-29 22:29:25 -0600

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>&#10023; SWAPI BOX &#10022;</h1>
+        <nav>
+          <h3>Favorites</h3>
+          <h3>Beings</h3>
+          <h3>Places</h3>
+          <h3>Vehicles</h3>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
