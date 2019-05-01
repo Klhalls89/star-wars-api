5be8bcf304385c29cb5a98e5a677c44a00651ca7
Kristen Hallstrom <klhalls89@gmail.com>
2019-04-30 20:48:55 -0600

    Fetch movie and set opening crawl

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
+// import Loader from '../Loader/Loader'
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>&#10023; SWAPI BOX &#10022;</h1>
-        <nav>
-          <h3>Favorites</h3>
-          <h3>Beings</h3>
-          <h3>Places</h3>
-          <h3>Vehicles</h3>
-        </nav>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movie: {},
+      beings: [],
+      places: [],
+      vehicles: [],
+      favorites: []
+
+    }
+  }
+
+  componentDidMount(){
+    this.getMovie()
+  }
+
+  getMovie = () => {
+    const url = "https://swapi.co/api/films"
+    fetch(url)
+      .then(response => response.json())
+      .then(results => this.setMovie(results.results))
+      .catch(error => console.log(error))
+  }
+
+  makeRandomNumber() {
+    return  Math.floor(Math.random() * (7))
+  }
+
+  setMovie = (movies) => {
+    const randomMovie = this.makeRandomNumber()
+    this.setState({
+      movie: movies[randomMovie]
+    })
+  }
+
+  render() {
+    const { title, opening_crawl, release_date} = this.state.movie
+    return (
+      <div className="App">
+        <header>
+          <h1>&#10023; SWAPI BOX &#10022;</h1>
+          <nav>
+            <h3>Favorites<span className="favoriteNumber">0</span></h3>
+            <h3>Beings</h3>
+            <h3>Places</h3>
+            <h3>Vehicles</h3>
+          </nav>
+        </header>
+          <marquee 
+            behavior="scroll" 
+            direction="up">
+            {title}....
+            {opening_crawl}
+            {release_date}
+          </marquee>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Loader() {
+  return(
+    <div class="loading-hourglass">I</div>
+  )
+}
+
+export default Loader;
